ff10cf8cb51263297291ef6efb38356d8324c54a
Veronika Turner <turner.veronika3@gmail.com>
2023-07-31 20:54:31 -0600

    Add Hamburger Icon

--- src/Common/PagesLayout/Header/Header.jsx
+++ src/Common/PagesLayout/Header/Header.jsx
@@ -18,45 +18,4 @@ const Header = () => {
 
 export default Header;
 
-const HeaderStyled = styled.header`
-    color: white;
-
-    .logo {
-        .SiteLogo {
-            width: 25%;
-            margin: auto;
-            padding: 20px 0px;
-        }
-    }
-
-    nav.mainMenu {
-        text-align: center;
-        a {
-            width: 80px;
-            display: inline-block;
-
-            line-height: 30px;
-            margin: 0px 0px 20px 5px;
-            border-bottom: solid 3px #84d4d4;
-
-            text-align: center;
-            font-size: 12px;
-            opacity: 0.8;
-            color: #84d4d4;
-            font-weight: bold;
-            text-decoration: none;
-            text-transform: uppercase;
-
-            &:hover,
-            &:active,
-            &:focus {
-                opacity: 1;
-            }
-
-            &.active {
-                color: white;
-                border-bottom-color: white;
-            }
-        }
-    }
-`;
+const HeaderStyled = styled.header``;
--- src/Common/PagesLayout/Header/Small.jsx
+++ src/Common/PagesLayout/Header/Small.jsx
@@ -1,11 +1,95 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+/* Components ---------------------------*/
+import SiteLogo from "../SiteLogo/SiteLogo.jsx";
+import Hamburger from "./Hamburger.jsx";
 
 const Small = () => {
-    return <SmallStyled className="Small">Small</SmallStyled>;
+    const [isMenuShowing, isMenuShowingUpdate] = useState(false);
+
+    const toggleMenu = () => {
+        isMenuShowingUpdate(!isMenuShowing);
+    };
+    const hideMenu = () => {
+        isMenuShowingUpdate(false);
+    };
+
+    useEffect(() => {
+        if (isMenuShowing) {
+            //add class modal-open
+            document.body.classList.add("modal-open");
+        } else {
+            //remove class modal-open
+            document.body.classList.remove("modal-open");
+        }
+        // on dismount clean up
+        return () => {
+            document.body.classList.remove("modal-open");
+        };
+    }, [isMenuShowing]);
+
+    return (
+        <SmallStyled className="Small">
+            <SiteLogo />
+            <Hamburger toggleMenu={toggleMenu} />
+
+            {isMenuShowing && (
+                <nav className="mainMenu">
+                    <NavLink onClick={hideMenu} to={"/"} end>
+                        Home
+                    </NavLink>
+                    <NavLink onClick={hideMenu} to={"/staff"}>
+                        Staff
+                    </NavLink>
+                    <NavLink onClick={hideMenu} to={"/contact"}>
+                        Contact
+                    </NavLink>
+                    <NavLink onClick={hideMenu} to={"/course-work"}>
+                        Course
+                    </NavLink>
+                </nav>
+            )}
+        </SmallStyled>
+    );
 };
 
 export default Small;
 
 const SmallStyled = styled.div`
     color: white;
+    display: flex;
+    justify-content: center;
+
+    .SiteLogo {
+        height: 300px;
+        padding: 20px;
+        max-width: 330px;
+    }
+
+    .mainMenu {
+        position: fixed;
+
+        left: 0px;
+        top: 300px;
+        right: 0px;
+        bottom: 0px;
+
+        background-color: #034444;
+
+        a {
+            display: block;
+            padding: 10px;
+            border-bottom: 1px solid #999999;
+            color: white;
+            text-decoration: none;
+
+            &:hover,
+            &:active,
+            &:focus {
+                background-color: #043434;
+            }
+        }
+    }
 `;
