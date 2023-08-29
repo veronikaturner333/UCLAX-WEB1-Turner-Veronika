9764d333165d83742654d0a3b513c323a29041d8
Veronika Turner <turner.veronika3@gmail.com>
2023-08-28 19:49:06 -0600

    S 10: Once Again

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,9 @@ export default defineConfig({
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
-    {
-        build: {
-            outDir: './build',
-        },
-        base: "/UCLAX-WEB1-Turner-Veronika/",
-    }
+
+    build: {
+        outDir: "./build",
+    },
+    base: "/UCLAX-WEB1-Turner-Veronika/",
 });
